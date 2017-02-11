51d10698eedb19906f4d6bf95ed6b855fd11f231
Jed Duffey <jedduffey@gmail.com>
2017-02-11 13:09:02 -0500

    pass third test

--- src/roman_numeral_converter_kata/RomanNumeralConverter.java
+++ src/roman_numeral_converter_kata/RomanNumeralConverter.java
@@ -3,12 +3,12 @@ package roman_numeral_converter_kata;
 public class RomanNumeralConverter {
 
 	public String convertArabicToRoman(int input) {
-		
-		
-		if(input == 1){
-			return "I";
-		}
-		return "V";
-	}
 
+		if (input == 1) {
+			return "I";
+		} else if (input == 5) {
+			return "V";
+		}
+		return "X";
+	}
 }
