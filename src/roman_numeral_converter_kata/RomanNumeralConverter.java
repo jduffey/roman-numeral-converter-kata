3bc3ec05f55b996c4c86fcbc5fefc19663dd0132
Jed Duffey <jedduffey@gmail.com>
2017-02-11 15:08:44 -0500

    got those to pass

--- src/roman_numeral_converter_kata/RomanNumeralConverter.java
+++ src/roman_numeral_converter_kata/RomanNumeralConverter.java
@@ -5,8 +5,8 @@ import java.util.HashMap;
 public class RomanNumeralConverter {
 
 	public String convertArabicToRoman(int input) {
-		
-		int inputCounter = input;
+
+		int counter = input;
 
 		HashMap<Integer, String> hmap = new HashMap<Integer, String>();
 
@@ -25,15 +25,72 @@ public class RomanNumeralConverter {
 		hmap.put(1000, "M");
 
 		String output = "";
-		
-		if (inputCounter < 4) {
 
-			for (int i = inputCounter; i > 0; i -= 1) {
-				output += hmap.get(1);
-			}
-			return output;
-		} else
+		while (counter >= 1000) {
+			output += hmap.get(1000);
+			counter -= 1000;
+		}
 
-		return hmap.get(input);
+		while (counter >= 900) {
+			output += hmap.get(900);
+			counter -= 900;
+		}
+
+		while (counter >= 500) {
+			output += hmap.get(500);
+			counter -= 500;
+		}
+
+		while (counter >= 400) {
+			output += hmap.get(400);
+			counter -= 400;
+		}
+
+		while (counter >= 100) {
+			output += hmap.get(100);
+			counter -= 100;
+		}
+
+		while (counter >= 90) {
+			output += hmap.get(90);
+			counter -= 90;
+		}
+
+		while (counter >= 50) {
+			output += hmap.get(50);
+			counter -= 50;
+		}
+
+		while (counter >= 40) {
+			output += hmap.get(40);
+			counter -= 40;
+		}
+
+		while (counter >= 10) {
+			output += hmap.get(10);
+			counter -= 10;
+		}
+
+		while (counter >= 9) {
+			output += hmap.get(9);
+			counter -= 9;
+		}
+
+		while (counter >= 5) {
+			output += hmap.get(5);
+			counter -= 5;
+		}
+
+		while (counter >= 4) {
+			output += hmap.get(4);
+			counter -= 4;
+		}
+
+		while (counter >= 1) {
+			output += hmap.get(1);
+			counter -= 1;
+		}
+
+		return output;
 	}
 }
