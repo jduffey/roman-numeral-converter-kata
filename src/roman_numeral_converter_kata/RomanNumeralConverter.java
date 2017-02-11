0e1e4bf0951eb9c401ec596c78efa82373ce0a0f
Jed Duffey <jedduffey@gmail.com>
2017-02-11 13:32:02 -0500

    begin to refactor

--- src/roman_numeral_converter_kata/RomanNumeralConverter.java
+++ src/roman_numeral_converter_kata/RomanNumeralConverter.java
@@ -1,8 +1,20 @@
 package roman_numeral_converter_kata;
 
+import java.util.HashMap;
+
 public class RomanNumeralConverter {
 
 	public String convertArabicToRoman(int input) {
+		
+		HashMap<Integer, String> hmap = new HashMap<Integer, String>();
+		
+		hmap.put(1, "I");
+		hmap.put(5, "V");
+		hmap.put(10, "X");
+		hmap.put(50, "L");
+		hmap.put(100, "C");
+		hmap.put(500, "D");
+		hmap.put(1000, "M");
 
 		if (input == 1) {
 			return "I";
