dbf4f9140d2718c4a48e354aa85f6a7b18110931
Jed Duffey <jedduffey@gmail.com>
2017-02-11 13:00:20 -0500

    create first method in class

--- src/roman_numeral_converter_kata/RomanNumeralConverter.java
+++ src/roman_numeral_converter_kata/RomanNumeralConverter.java
@@ -2,4 +2,9 @@ package roman_numeral_converter_kata;
 
 public class RomanNumeralConverter {
 
+	public String convertArabicToRoman(int i) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
 }
